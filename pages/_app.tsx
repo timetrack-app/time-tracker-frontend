3b80a5f88020daf86e572b4bd22593c94791ff98
koyaaoyama <aoyama.ko0920@gmail.com>
2023-08-28 22:58:42 -0700

    Apply ThemeProvider of styled-components, Provider of redux. Make it possible to use getLayout()

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,71 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import { useEffect, ReactElement, ReactNode } from 'react';
 
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
+// Next
+import { NextPage } from 'next';
+import { Router } from 'next/router';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
 
-export default function App({ Component, pageProps }: AppProps) {
+// Libraries
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+
+import { store } from '../stores/store';
+
+import GlobalStyle from '../components/globalstyles';
+
+import useColorTheme from '../hooks/useColorTheme';
+
+// Layout configuration doc
+// https://nextjs.org/docs/pages/building-your-application/routing/pages-and-layouts#with-typescript
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+  router: Router // Error if this property doesn't exist
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+/**
+ *
+ *
+ * @param {AppPropsWithLayout} { Component, pageProps }
+ * @return {*} JSX.Element
+ */
+const WithThemeProviderComponent = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const { initColorTheme, getCurrentColorThemeStyle } = useColorTheme();
+
+  useEffect(() => {
+    initColorTheme();
+  }, []);
+
   return (
-    <>
-      <ThemeProvider theme={theme}>
+    <div className={inter.className}>
+      <ThemeProvider theme={getCurrentColorThemeStyle()}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
-  )
-}
+    </div>
+  );
+};
+
+const App = ({ Component, pageProps, router }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <Provider store={store}>
+      {getLayout(
+        <WithThemeProviderComponent
+          Component={Component}
+          pageProps={pageProps}
+          router={router}
+        />,
+      )}
+    </Provider>
+  );
+};
+export default App;
