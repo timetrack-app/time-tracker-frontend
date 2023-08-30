0ddb7fd35e07bf1ab799cb4dbc7c07df68606a59
koyaaoyama <aoyama.ko0920@gmail.com>
2023-08-30 16:38:07 -0700

    Fix actions in activeTaskSlice

--- stores/slices/activeTaskSlice.ts
+++ stores/slices/activeTaskSlice.ts
@@ -5,20 +5,39 @@ import type { RootState } from '../store';
 type ActiveTaskState = {
   name: string
   elapsedSeconds: number
+  isTimerRunning: boolean
 };
 
 const initialState: ActiveTaskState = {
   name: '',
   elapsedSeconds: 0,
+  isTimerRunning: false,
 };
 
 const activeTaskSlice = createSlice({
   name: 'activeTask',
   initialState,
   reducers: {
-    updateActiveTask: (state, action: PayloadAction<ActiveTaskState>) => {
-      state.name = action.payload.name;
-      state.elapsedSeconds = action.payload.elapsedSeconds;
+    resetActiveTask: (state) => {
+      state.name = '';
+      state.elapsedSeconds = 0;
+      state.isTimerRunning = false;
+    },
+    resetTimer: (state) => {
+      state.elapsedSeconds = 0;
+      state.isTimerRunning = false;
+    },
+    updateActiveTaskName: (state, action: PayloadAction<string>) => {
+      state.name = action.payload;
+    },
+    updateIsTimerRunning: (state, action: PayloadAction<boolean>) => {
+      state.isTimerRunning = action.payload;
+    },
+    updateElapsedSeconds: (state, action: PayloadAction<number>) => {
+      state.elapsedSeconds = action.payload;
+    },
+    incrementElapsedSeconds: (state) => {
+      state.elapsedSeconds += 1;
     },
   },
 });
@@ -28,7 +47,12 @@ export const selectActiveTask = (state: RootState) => state.activeTask;
 
 // actions
 export const {
-  updateActiveTask,
+  resetActiveTask,
+  updateActiveTaskName,
+  updateElapsedSeconds,
+  updateIsTimerRunning,
+  incrementElapsedSeconds,
+  resetTimer,
 } = activeTaskSlice.actions;
 
 export default activeTaskSlice.reducer;
