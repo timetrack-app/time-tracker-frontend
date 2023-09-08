9a8aae4f8d31d6e9ed35042e8e0f6da9e495f77f
koyaaoyama <aoyama.ko0920@gmail.com>
2023-09-07 22:14:36 -0700

    Fix validation messages

--- const/validation/messages/index.ts
+++ const/validation/messages/index.ts
@@ -2,13 +2,13 @@
 // If it gets too large then separate into multiple files.
 
 export const emailRequired = 'Email is required';
-export const emailInvalid = 'Please enter a valid email.';
+export const emailInvalid = 'Please enter a valid email';
 
-export const passwordLength = 'Password must be 6-20 characters.';
-export const passwordNumber = 'Password must contain at least one number.';
-export const passwordUppercase = 'Password must have at least one uppercase letter.';
-export const passwordLowercase = 'Password must have at least one lowercase letter.';
+export const passwordLength = 'Password must be 6-20 characters';
+export const passwordNumber = 'Password must contain at least one number';
+export const passwordUppercase = 'Password must have at least one uppercase letter';
+export const passwordLowercase = 'Password must have at least one lowercase letter';
 export const passwordRequired = 'Password is required.';
 
-export const passwordConfirmationMismatch = 'Passwords do not match.';
-export const passwordConfirmationRequired = 'Password confirmation is required.';
+export const passwordConfirmationMismatch = 'Passwords do not match';
+export const passwordConfirmationRequired = 'Password confirmation is required';
