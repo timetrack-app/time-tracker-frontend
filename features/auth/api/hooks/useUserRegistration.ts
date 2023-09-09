054679c0a1aaa7b983f3d9e0272781abb4f9bfa6
koyaaoyama <aoyama.ko0920@gmail.com>
2023-09-09 14:59:04 -0700

    Delete features/auth/api/services/registerUser.ts. Include api call function within useUserRegistration

--- features/auth/api/hooks/useUserRegistration.ts
+++ features/auth/api/hooks/useUserRegistration.ts
@@ -1,6 +1,21 @@
 import { useMutation } from 'react-query';
-import { registerUser } from '../services/registerUser';
+import { axiosBase } from '../../../../libs/axios';
+import { getApiEndpointFull } from '../../../../routes/api';
 import { UserRegistrationRequest } from '../../types';
+
+/**
+ * User registration API call
+ *
+ * @param {UserRegistrationRequest} values
+ * @return {*}  {Promise<void>}
+ */
+const registerUser = async (
+  values: UserRegistrationRequest,
+): Promise<void> => {
+  const res = await axiosBase().post(getApiEndpointFull('register'), values);
+
+  return res.data;
+};
 
 /**
  * Custom hook for user registration
--- features/auth/types/index.ts
+++ features/auth/types/index.ts
@@ -3,8 +3,17 @@ export type UserRegistrationRequest = {
   password: string
 };
 
-export type UserRegistrationResponse = {
-  id: number
+// export type UserRegistrationResponse = {
+//   id: number
+//   email: string
+//   token: string
+// };
+
+export type UserLoginRequest = {
   email: string
+  password: string
+};
+
+export type UserLoginResponse = {
   token: string
 };
