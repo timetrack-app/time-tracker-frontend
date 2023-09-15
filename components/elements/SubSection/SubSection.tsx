7f56681d5b4d7922ebd39d6b4c96fc027798ade4
koyaaoyama <aoyama.ko0920@gmail.com>
2023-09-14 17:14:12 -0700

    [WIP] SubSection. Markup done

---
+++ components/elements/SubSection/SubSection.tsx
@@ -0,0 +1,110 @@
+import styled from 'styled-components';
+
+import ButtonDanger from '../Button/ButtonDanger';
+
+import { useAppSelector } from '../../../stores/hooks';
+import { selectColorTheme } from '../../../stores/slices/colorThemeSlice';
+import { secondsToHHMMSS } from '../../../utils/timer';
+
+import { ColorThemeName } from '../../../types/colorTheme';
+import { white, coralRed } from '../../../const/styles/colors';
+
+const ContainerDiv = styled.div<{ colorThemeName: ColorThemeName }>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5em;
+  border-radius: 40px;
+  padding: 2em;
+  background-color: ${({ theme }) => theme.colors.componentBackground};
+  box-shadow: ${({ colorThemeName, theme }) => (colorThemeName === 'light' ? `0 5px 6px 0 ${theme.colors.border}` : 'none')};
+`;
+
+const SectionDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5em;
+`;
+
+const SectionTitleWrapperDiv = styled.div`
+  width: 100%;
+  padding: 0.3em 1em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${({ theme }) => theme.colors.text};
+  border-radius: 16px;
+`;
+
+const SectionTitleP = styled.p`
+  font-size: 1.3em;
+  font-weight: 500;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const ElapsedTimeP = styled.p`
+  display: block;
+  font-size: 3em;
+  font-weight: bold;
+  max-width: 100%;
+  text-align: center;
+`;
+
+const ButtonTextP = styled.p`
+  display: block;
+  color: ${white};
+  font-weight: 500;
+`;
+
+const ButtonCustom = styled(ButtonDanger)`
+  padding: 0.5em 1em;
+`;
+
+type SubSectionProps = {
+  totalSeconds: number
+  selectedTabName: string
+  totalSecondsOfSelectedTab: number
+};
+
+const SubSection = ({
+  totalSeconds,
+  selectedTabName,
+  totalSecondsOfSelectedTab,
+}: SubSectionProps) => {
+  const currentColorTheme = useAppSelector(selectColorTheme);
+
+  return (
+    <ContainerDiv colorThemeName={currentColorTheme}>
+      <SectionDiv>
+        <SectionTitleWrapperDiv>
+          <SectionTitleP>Total Time</SectionTitleP>
+        </SectionTitleWrapperDiv>
+        <ElapsedTimeP>{secondsToHHMMSS(totalSeconds)}</ElapsedTimeP>
+      </SectionDiv>
+
+      <SectionDiv>
+        <SectionTitleWrapperDiv>
+          <SectionTitleP>{`${selectedTabName} Total Time`}</SectionTitleP>
+        </SectionTitleWrapperDiv>
+        <ElapsedTimeP>{secondsToHHMMSS(totalSecondsOfSelectedTab)}</ElapsedTimeP>
+      </SectionDiv>
+
+      <ButtonCustom
+        color={coralRed}
+        borderColor={coralRed}
+        backgroundColor={coralRed}
+      >
+        <ButtonTextP>End this session</ButtonTextP>
+      </ButtonCustom>
+    </ContainerDiv>
+  );
+};
+
+export default SubSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,17 @@ import { useAppDispatch, useAppSelector } from '../stores/hooks';
 import { selectActiveTask, updateActiveTaskName } from '../stores/slices/activeTaskSlice';
 
 import MainTimer from '../components/elements/MainTimer/MainTimer';
+import SubSection from '../components/elements/SubSection/SubSection';
 
 // Tmp style
 const TestContainer = styled.div`
   width: 310px;
   height: 310px;
+  margin-bottom: 50px;
+`;
+
+const SubSectionWrapper = styled.div`
+  width: 310px;
 `;
 
 const Home = () => {
@@ -22,9 +28,22 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <TestContainer>
-      <MainTimer taskName={name} isTimerRunning={isTimerRunning} elapsedSeconds={elapsedSeconds} />
-    </TestContainer>
+    <>
+      <TestContainer>
+        <MainTimer
+          taskName={name}
+          isTimerRunning={isTimerRunning}
+          elapsedSeconds={elapsedSeconds}
+        />
+      </TestContainer>
+      <SubSectionWrapper>
+        <SubSection
+          totalSeconds={8000}
+          selectedTabName="Tab name here"
+          totalSecondsOfSelectedTab={5400}
+        />
+      </SubSectionWrapper>
+    </>
   );
 };
 
