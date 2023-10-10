7be5d852e1f476043d53cec905e09cae241d2a86
koyaaoyama <aoyama.ko0920@gmail.com>
2023-10-09 21:07:47 -0700

    Move test tab data to dummyData.ts

---
+++ components/pages/home/dummyData.ts
@@ -0,0 +1,99 @@
+export const testTabs = [
+  {
+    id: 1,
+    name: 'Tab 1',
+    displayOrder: 1,
+    taskLists: [
+      {
+        id: 1,
+        name: 'Task List 1',
+        displayOrder: 1,
+        tasks: [
+          {
+            id: 1,
+            displayOrder: 1,
+            name: 'Task 1',
+            description: 'Description for Task 1',
+            totalTime: 30,
+          },
+          {
+            id: 2,
+            displayOrder: 2,
+            name: 'Task 2',
+            description: 'Description for Task 2',
+            totalTime: 45,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'Task List 2',
+        displayOrder: 2,
+        tasks: [
+          {
+            id: 3,
+            displayOrder: 1,
+            name: 'Task 3',
+            description: 'Description for Task 3',
+            totalTime: 20,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Tab 2',
+    displayOrder: 2,
+    taskLists: [
+      {
+        id: 3,
+        name: 'Task List 3',
+        displayOrder: 1,
+        tasks: [
+          {
+            id: 4,
+            displayOrder: 1,
+            name: 'Task 4',
+            description: 'Description for Task 4',
+            totalTime: 15,
+          },
+        ],
+      },
+      // You can add more task lists for Tab 2 if needed
+    ],
+  },
+  {
+    id: 3,
+    name: 'Tab 3',
+    displayOrder: 3,
+    taskLists: [
+      // Define task lists for Tab 3 here if needed
+    ],
+  },
+  // Add more tabs as needed
+  {
+    id: 4,
+    name: 'Tab 4',
+    displayOrder: 4,
+    taskLists: [
+      // Define task lists for Tab 3 here if needed
+    ],
+  },
+  {
+    id: 5,
+    name: 'Tab 5',
+    displayOrder: 5,
+    taskLists: [
+      // Define task lists for Tab 3 here if needed
+    ],
+  },
+  {
+    id: 6,
+    name: 'Tab 6',
+    displayOrder: 6,
+    taskLists: [
+      // Define task lists for Tab 3 here if needed
+    ],
+  },
+];
