ea6ad67e0904de93cab0d2b8b71bffdad0590128
koyaaoyama <aoyama.ko0920@gmail.com>
2023-10-28 15:59:58 -0700

    Separate dashboard layout into its own component

--- components/pages/dashboard/DashboardPage.tsx
+++ components/pages/dashboard/DashboardPage.tsx
@@ -1,51 +1,16 @@
-import Link from 'next/link';
-import styled from 'styled-components';
-import { FiChevronLeft } from 'react-icons/fi';
 import { FaUserAlt } from 'react-icons/fa';
 import { LuLayoutTemplate } from 'react-icons/lu'
 import { useAppSelector } from '../../../stores/hooks';
 import { selectColorTheme } from '../../../stores/slices/colorThemeSlice';
 import { ContentsCard, Content } from '../../../features/dashboard';
 import { getWebRoute } from '../../../routes/web';
+import DashboardLayout from '../../../features/dashboard/components/layouts/DashboardLayout';
 
-const MainContainerDiv = styled.div`
-`;
-
-const ContentsContainerDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.2em;
-  max-width: 50em;
-  min-height: 50em;
-  margin: 0px auto;
-  padding: 32px 8px;
-`;
-
-const Header = styled.header`
-  height: 6em;
-  width: 100%;
-  padding: 1em 0;
-`;
-
-const BackButtonDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: none;
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 2em;
-  background-color: ${({ theme }) => theme.colors.background};
-  width: 2em;
-  height: 2em;
-  cursor: pointer;
-  border-radius: 50%;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.componentBackground};
-  }
-`;
-
+/**
+ * Dashboard index
+ *
+ * @return {JSX.Element}
+ */
 const DashboardPage = () => {
   const currentColorThemeName = useAppSelector(selectColorTheme);
 
@@ -54,26 +19,15 @@ const DashboardPage = () => {
   // TODO: template detail page(with delete button)
 
   return (
-    <MainContainerDiv>
-      <ContentsContainerDiv>
-        <Header>
-          <Link href={getWebRoute('home')}>
-            <BackButtonDiv>
-              <FiChevronLeft />
-            </BackButtonDiv>
-          </Link>
-        </Header>
+    <DashboardLayout backButtonHref={getWebRoute('home')}>
+      <ContentsCard title='Account' colorThemeName={currentColorThemeName}>
+        <Content title='Edit profile' href='#' icon={<FaUserAlt />} />
+      </ContentsCard>
 
-        <ContentsCard title='Account' colorThemeName={currentColorThemeName}>
-          <Content title='Edit profile' href='#' icon={<FaUserAlt />} />
-        </ContentsCard>
-
-        <ContentsCard title='Application' colorThemeName={currentColorThemeName}>
-          <Content title='Templates' href='#' icon={<LuLayoutTemplate />} />
-        </ContentsCard>
-
-      </ContentsContainerDiv>
-    </MainContainerDiv>
+      <ContentsCard title='Application' colorThemeName={currentColorThemeName}>
+        <Content title='Templates' href='#' icon={<LuLayoutTemplate />} />
+      </ContentsCard>
+    </DashboardLayout>
   );
 };
 
--- features/dashboard/index.ts
+++ features/dashboard/index.ts
@@ -1,2 +1,3 @@
 export { default as ContentsCard } from './components/elements/ContentsCard/ContentsCard';
 export { default as Content } from './components/elements/ContentsCard/Content';
+export { default as DashboardLayout } from './components/layouts/DashboardLayout';
