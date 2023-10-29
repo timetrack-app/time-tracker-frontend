f6cae89ee632583bda70da5b461c43f0b561a8d9
koyaaoyama <aoyama.ko0920@gmail.com>
2023-10-29 16:59:30 -0700

    Fix RHF component exports

--- components/elements/ReactHookForm/index.ts
+++ components/elements/ReactHookForm/index.ts
@@ -1,5 +1,6 @@
 import TextInput from './TextInput';
 import Form from './Form';
 import FieldWrapper from './FieldWrapper';
+import type { FormProps } from './types';
 
-export { TextInput, Form, FieldWrapper };
+export { TextInput, Form, FieldWrapper, FormProps };
