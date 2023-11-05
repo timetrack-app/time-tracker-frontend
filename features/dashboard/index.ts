e598ffe74558b93edb0e26ff06bac935e5df704c
koyaaoyama <aoyama.ko0920@gmail.com>
2023-11-05 14:19:59 -0800

    Implement PasswordUpdaePage

--- features/dashboard/index.ts
+++ features/dashboard/index.ts
@@ -1,3 +1,5 @@
 export { default as ContentsCard } from './components/elements/ContentsCard/ContentsCard';
 export { default as Content } from './components/elements/ContentsCard/Content';
 export { default as DashboardLayout } from './components/layouts/DashboardLayout';
+export { default as ProfileForm } from './components/elements/ProfileForm';
+export { default as PasswordInput} from './components/elements/PasswordInput';
