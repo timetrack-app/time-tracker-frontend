af3790bd7d7f91a4e8364c4ef997a5c9cb9c22cf
koyaaoyama <aoyama.ko0920@gmail.com>
2024-02-03 23:31:54 -0800

    Rename AuthButton -> SubmitButton

--- features/auth/components/elements/AuthButton.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { ButtonPrimary } from '../../../../components/elements/common';
-
-const AuthButton = styled(ButtonPrimary)`
-  width: 30%;
-`;
-
-export default AuthButton;
