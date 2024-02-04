1f2585681c04461511e050169075aefcac5cb602
koyaaoyama <aoyama.ko0920@gmail.com>
2024-02-03 22:55:21 -0800

    Implement AuthButton component for login/signup form

---
+++ features/auth/components/elements/AuthButton.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { ButtonPrimary } from '../../../../components/elements/common';
+
+const AuthButton = styled(ButtonPrimary)`
+  width: 30%;
+`;
+
+export default AuthButton;
--- features/auth/index.ts
+++ features/auth/index.ts
@@ -1,6 +1,7 @@
 export { default as AuthFormLayout } from './components/layouts/AuthFormLayout';
 export { default as AuthForm } from './components/elements/AuthForm';
 export { default as AuthFormContentsWrapper } from './components/elements/AuthFormContentsWrapper';
+export { default as AuthButton } from './components/elements/AuthButton';
 export * from './api/hooks/useUserRegistration';
 export * from './api/hooks/useUserLogin';
 export * from './api/hooks/useIsAuthenticated';
